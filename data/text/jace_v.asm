34ccc9de16539fdebcefd617fc7fded9e98421b5
TempoQuill <jicrapo@gmail.com>
2022-09-05 16:04:27 -0400

    Add Jace V trainer messages

---
+++ data/text/jace_v.asm
@@ -0,0 +1,37 @@
+JJTrainerEncounter:
+	text "Hiya! You're"
+	line "<PLAYER>, right?"
+
+	para "Great! Do you time"
+	line "the evolutions of"
+	cont "your #MON?"
+
+	para "You can get better"
+	line "moves earlier if"
+
+	para "you cancel the"
+	line "evolutionary"
+
+	para "process enough"
+	line "times."
+	done
+
+JJTrainerDefeated:
+	text "Gah! You're way too"
+	line "much!"
+	done
+
+JJTrainerAfter:
+	text "Tell nobody about"
+	line "what I'm about to"
+	cont "say!"
+
+	para "I drew you!"
+
+	para "I honestly thought"
+	line "timing evolutions"
+	cont "could work."
+
+	para "You sure showed me"
+	line "wrong."
+	done
